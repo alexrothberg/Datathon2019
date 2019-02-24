31913bd78f832127a744de275b47ff5faaebd7d3
David Charatan <david.charatan@gmail.com>
2019-02-23 22:29:21 -0500

    Add QR decoding

--- Assets/ReadQr.cs
+++ Assets/ReadQr.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 using ZXing;
 using ZXing.QrCode;
 
+public struct QRData
+{
+    public int[] heights;
+    public int[] colors;
+
+    public QRData(int[] heights, int[] colors)
+    {
+        this.heights = heights;
+        this.colors = colors;
+    }
+}
 
 public class ReadQr : MonoBehaviour
 {
@@ -32,26 +43,57 @@ public class ReadQr : MonoBehaviour
         // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
         try
         {
+            // Decodes the current frame.
             IBarcodeReader barcodeReader = new BarcodeReader();
-            // decode the current frame
-            var result = barcodeReader.Decode(camTexture.GetPixels32(),
-              camTexture.width, camTexture.height);
-            if (result != null)
+            var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width, camTexture.height);
+            if(result != null)
             {
-                Debug.Log("DECODED TEXT FROM QR: " +result.Text);
-                char[] chararr = result.Text.ToCharArray();
-                byte[] byteArr = ASCIIEncoding.ASCII.GetBytes(result.Text);
+                // Decodes the QR code.
+                Debug.Log("Decoded from QR: " + result.Text);
+                QRData qrData = DecodeTextQR(result.Text);
 
-                String logOutput = "";
-                foreach (byte b in byteArr)
+                // Creates a debug string.
+                String debugHeights = "Heights: ";
+                String debugColors = "Colors: ";
+                for(int i = 0; i < 50; i++)
                 {
-                    logOutput += (b + " ");
+                    debugHeights += qrData.heights[i] + " ";
+                    debugColors += qrData.colors[i] + " ";
                 }
-                Debug.Log(logOutput);
+                Debug.Log(debugHeights);
+                Debug.Log(debugColors);
             }
         }
         catch (Exception ex) { Debug.LogWarning(ex.Message); }
     }
+
+    /// <summary>
+    /// Turns a QR string into a QRData struct.
+    /// </summary>
+    /// <returns>QRData struct</returns>
+    /// <param name="qr">QR data string</param>
+    public QRData DecodeTextQR(String qr)
+    {
+        String encoding = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.";
+        int[] heights = new int[50];
+        int[] colors = new int[50];
+
+        // Decodes the heights.
+        for(int i = 0; i < 50; i++)
+        {
+            heights[i] = encoding.IndexOf(qr[i]);
+        }
+
+        // Decodes the colors.
+        for (int i = 0; i < 50; i++)
+        {
+            heights[i] = encoding.IndexOf(qr[i + 50]);
+        }
+
+        return new QRData(heights, colors);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
