77c40da051425b30ddd434c49ed8827430d4d059
alexrothberg <36311902+alexrothberg@users.noreply.github.com>
2019-02-24 03:18:30 -0500

    Revert "."
    This reverts commit c530f16952a2809e5d8ffc4d90930462ec5c53e5.

--- Assets/ReadQr.cs
+++ Assets/ReadQr.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using Vuforia;
 using ZXing;
 using ZXing.QrCode;
 
@@ -32,7 +31,7 @@ public class ReadQr : MonoBehaviour
         camTexture = new WebCamTexture();
         camTexture.requestedHeight = Screen.height;
         camTexture.requestedWidth = Screen.width;
-
+        
         if (camTexture != null)
         {
             camTexture.Play();
@@ -49,7 +48,7 @@ public class ReadQr : MonoBehaviour
             // Decodes the current frame.
             IBarcodeReader barcodeReader = new BarcodeReader();
             var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width, camTexture.height);
-            if (result != null)
+            if(result != null)
             {
                 // Decodes the QR code.
                 Debug.Log("Decoded from QR: " + result.Text);
@@ -58,7 +57,7 @@ public class ReadQr : MonoBehaviour
                 // Creates a debug string.
                 String debugHeights = "Heights: ";
                 String debugColors = "Colors: ";
-                for (int i = 0; i < 50; i++)
+                for(int i = 0; i < 50; i++)
                 {
                     debugHeights += qrData.heights[i] + " ";
                     debugColors += qrData.colors[i] + " ";
@@ -108,18 +107,9 @@ public class ReadQr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            if (camTexture.isPlaying)
-            {
-                camTexture.Pause();
-            }
-            else
-            {
-                camTexture.Play();
-            }
-        }
+       
+
     }
 
 
-}
+   }
